eaca36bf027c212f7c28b7cbc4a18f840204efca
Seth Kinast <skinast@linkedin.com>
2014-11-14 21:10:39 -0800

    Fixed code

--- frontend/js/issueApp.js
+++ frontend/js/issueApp.js
@@ -1,18 +1,18 @@
 angular.module("issue_form", [])
-.controller("baseCtrl", "$scope", function($scope) {
+.controller("baseCtrl", ["$scope", function($scope) {
     $scope.getData = function() {
         console.log($scope.dust_issue.base.works);
     }
-})
-.controller("nestedBreaksCtrl", "$scope", function($scope) {
+}])
+.controller("nestedBreaksCtrl", ["$scope", function($scope) {
     $scope.getData = function() {
         console.log($scope.dust_issue.nested.breaks);
     }
-})
-.controller("nestedWorksCtrl", "$scope", function($scope) {
+}])
+.controller("nestedWorksCtrl", ["$scope", function($scope) {
     $scope.dust_issue = {};
     $scope.dust_issue.nested = {};
     $scope.getData = function() {
         console.log($scope.dust_issue.nested.works);
     }
-});
+}]);
